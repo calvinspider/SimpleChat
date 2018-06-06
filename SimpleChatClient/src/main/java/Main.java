f0a4fd31e3d621d8a541d2f94dde22f8df64fb95
calvinzhang <calvin4317991@sohu.com>
2018-06-06 22:50:52 +0800

    1

--- SimpleChatClient/src/main/java/Main.java
+++ SimpleChatClient/src/main/java/Main.java
@@ -3,17 +3,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import static javafx.stage.StageStyle.TRANSPARENT;
+import static javafx.stage.StageStyle.UTILITY;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setScene(new Scene(root, 430, 330));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
