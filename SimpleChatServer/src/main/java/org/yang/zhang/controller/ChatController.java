447e62cdbab8a9b7970bb2a0bbdf13b1c8776af0
calvinzhang <530678742@qq.com>
2018-06-21 22:33:40 +0800

    1

--- SimpleChatServer/src/main/java/org/yang/zhang/controller/ChatController.java
+++ SimpleChatServer/src/main/java/org/yang/zhang/controller/ChatController.java
@@ -1,5 +1,6 @@
 package org.yang.zhang.controller;
 
+import org.yang.zhang.entity.Contract;
 import org.yang.zhang.entity.User;
 import org.yang.zhang.service.ChatService;
 import org.yang.zhang.entity.Result;
@@ -7,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author calvin.zhang
@@ -19,6 +23,18 @@ public class ChatController {
 
     @Autowired
     private ChatService chatService;
+
+
+    @RequestMapping(value = "/contract")
+    public List<Contract> getContractList(String userId)
+    {
+        List<Contract> list=new ArrayList<>();
+        Contract contract=new Contract();
+        contract.setUserId("10001");
+        contract.setUserName("USER1");
+        list.add(contract);
+        return list;
+    }
 
     @RequestMapping(value = "/getfriendlist")
     public Result getfriendlist(@RequestBody User user)
